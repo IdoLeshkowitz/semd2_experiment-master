890cd0c92c740dde661b84ef76a5f246a09f39df
IdoLeshkowitz <idoleshkowitz@gmail.com>
2023-06-06 15:11:10 +0300

    fixed total payment

--- exit/__init__.py
+++ exit/__init__.py
@@ -1,6 +1,5 @@
 from otree.api import *
 
-
 doc = """
 Your app description
 """
@@ -21,45 +20,57 @@ class Group(BaseGroup):
 
 
 class Player(BasePlayer):
-    full_name = models.StringField(
-        label = "Full Name:",
-        blank = False
-    )
-    email = models.StringField(
-        label = "Email Address:",
-        blank = False,
-    )
+    full_name = models.StringField(label="Full Name:", blank=False)
+    email = models.StringField(label="Email Address:", blank=False, )
+    total_payment = models.FloatField(initial=0)
+
+
+def get_understanding_bonus_ratio(understanding_bonus, max_understanding_bonus):
+    return round(understanding_bonus / max_understanding_bonus * 100, 1)
+
+
+def get_understanding_bonus_money(understanding_bonus_ratio):
+    return round((understanding_bonus_ratio / 100) * 4, 2)
+
+
+def get_total_payment(understanding_bonus_money, payoff):
+    return float(understanding_bonus_money + payoff)
 
 
 # PAGES
 class EndSurvey(Page):
     form_model = 'player'
-    form_fields = ['full_name','email']
+    form_fields = ['full_name', 'email']
+
     # calculate total payoff and give code
     @staticmethod
     def vars_for_template(player: Player):
         total_payoff = player.participant.payoff_plus_participation_fee()
-        understanding_bonus_ratio = round(player.participant.understanding_bonus / player.participant.max_understanding_bonus*100,2)
-        understanding_bonus_money = round((understanding_bonus_ratio / 100)* 4,2)
+        understanding_bonus_ratio = get_understanding_bonus_ratio(player.participant.understanding_bonus, player.participant.max_understanding_bonus)
+        understanding_bonus_money = get_understanding_bonus_money(understanding_bonus_ratio)
+        total_payment = get_total_payment(understanding_bonus_money, total_payoff)
         return {
-            "stam":player.participant.understanding_bonus,
+            "stam":                      player.participant.understanding_bonus,
             "understanding_bonus_ratio": understanding_bonus_ratio,
             "understanding_bonus_money": understanding_bonus_money,
-            "total_payoff": total_payoff,
-            "completion_code": "5682D891"
+            "total_payoff":              total_payoff,
+            "total_payment":             total_payment,
+            "completion_code":           "5682D891"
         }
 
     @staticmethod
     def is_displayed(player: Player):
         return player.participant.consent == True
 
-
     @staticmethod
     def before_next_page(player: Player, timeout_happened):
         player.participant.full_name = player.full_name
         player.participant.email = player.email
+        player.total_payment = get_total_payment(get_understanding_bonus_money(get_understanding_bonus_ratio(player.participant.understanding_bonus, player.participant.max_understanding_bonus)), player.participant.payoff_plus_participation_fee())
 
 
 class ThankYou(Page):
     pass
-page_sequence = [EndSurvey,ThankYou]
+
+
+page_sequence = [EndSurvey, ThankYou]
