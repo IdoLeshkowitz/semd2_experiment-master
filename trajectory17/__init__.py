8041409e591b23b2723e4b88253944688c2418a0
IdoLeshkowitz <idoleshkowitz@gmail.com>
2023-07-17 18:58:14 +0300

    added runtime__variant

--- trajectory17/__init__.py
+++ trajectory17/__init__.py
@@ -38,5 +38,6 @@ class PassVars(Page):
         player.participant.understanding_bonus_limit = 0
         player.participant.understanding_bonus = 0
         player.participant.runtime__variant = "null"
+        player.participant.runtime__treatment = "null"
 
 page_sequence = [PassVars]
