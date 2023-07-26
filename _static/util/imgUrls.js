3bf0a69a6b569eff91e982e18a63f4a685868fae
IdoLeshkowitz <idoleshkowitz@gmail.com>
2023-07-26 18:53:06 +0300

    photos

--- _static/util/imgUrls.js
+++ _static/util/imgUrls.js
@@ -2,3 +2,8 @@ const imgUrls = {
     "trad":"https://semd2.s3.eu-north-1.amazonaws.com/trad-flow.png",
     "menu":"https://semd2.s3.eu-north-1.amazonaws.com/menu-flow.png"
 }
+
+const videoUrls = {
+    "menuMechanics":"https://semd2.s3.eu-north-1.amazonaws.com/Menu+Mechanics.mp4",
+    "traditionalMechanics":"https://semd2.s3.eu-north-1.amazonaws.com/Traditional+Mechanics.mp4"
+}
